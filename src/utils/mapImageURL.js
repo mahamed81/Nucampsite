2a26dbf2a36a42ff2d304a9648ecda6552638386
mahamed81 <mahamed10ahmed@gmail.com>
2022-08-17 20:53:16 -0500

    added Fetch and createAsyncThunk

---
+++ src/utils/mapImageURL.js
@@ -0,0 +1,11 @@
+import {baseUrl} from '/Users/mahamedahmed/Desktop/NucampFolder/3-React/nucampsite/src/app/shared/baseUrl.js'
+
+
+export const mapImageURL = (arr) => {
+  return arr.map((item) => {
+      return {
+          ...item,
+          image: baseUrl + item.image
+      };
+  });
+};
